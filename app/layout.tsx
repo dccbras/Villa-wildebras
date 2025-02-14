02ad2b3d3a281019e086928b10c860f441c3f808
dccbras <dccbras@gmail.com>
2025-02-14 20:15:54 +0100

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import localFont from "next/font/local"
+import "./globals.css"
+import type React from "react"
+
+const inter = Inter({ subsets: ["latin"] })
+const waterlily = localFont({ src: "./fonts/waterlily.ttf" }) // Aangepast naar .ttf
+
+export const metadata: Metadata = {
+  title: "Villa Wildebras - verblijf in ons unieke tiny house",
+  description: "Geniet van een heerlijk verblijf aan de rand van het Noordhollands Duinreservaat",
+    generator: 'v0.dev'
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <style jsx global>{`
+          h1, h2, h3, h4, h5, h6 {
+            font-family: ${waterlily.style.fontFamily};
+          }
+        `}</style>
+        {children}
+      </body>
+    </html>
+  )
+}
+
+
+
+import './globals.css'
---
+++ app/page.tsx
@@ -0,0 +1,4 @@
+export default function Page() {
+  return <main>{/* Add your page content here */}</main>
+}
+
