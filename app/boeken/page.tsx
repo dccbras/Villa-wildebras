67836cd74a7b9269c781ef14c4ad9fdbed30b541
dccbras <dccbras@gmail.com>
2025-02-19 21:33:17 +0100

    Update page.tsx

--- app/boeken/page.tsx
+++ app/boeken/page.tsx
@@ -1,25 +1,12 @@
-import React, { useEffect, useRef } from 'react';
+import BookingWidget from "./BookingWidget";
 
-function BookingWidget() {
-    const widgetContainerRef = useRef(null);
-
-    useEffect(() => {
-        const script = document.createElement('script');
-        script.async = true;
-        script.src = "//widget.simplybook.it/v2/widget/widget.js";
-        script.onload = () => {
-            new SimplybookWidget({"widget_type":"iframe","url":"https:\/\/villawildebras.simplybook.it","theme":"belle","theme_settings":{"timeline_hide_unavailable":"1","hide_past_days":"0","timeline_show_end_time":"0","timeline_modern_display":"as_slots","sb_base_color":"#7b8a78","display_item_mode":"block","sb_review_image":"","dark_font_color":"#474747","light_font_color":"#ffffff","btn_color_1":"#b0c3ac","sb_company_label_color":"#616760","hide_img_mode":"0","show_sidebar":"1","sb_busy":"#c7b3b3","sb_available":"#d6ebff"},"timeline":"modern","datepicker":"top_calendar","is_rtl":false,"app_config":{"clear_session":0,"allow_switch_to_ada":0,"predefined":[]},"container_id":"sbw_t7912b"});
-        };
-        document.head.appendChild(script);
-
-        return () => {
-            document.head.removeChild(script);
-        };
-    }, []);
-
-    return (
-       <div id="sbw_t7912b" ref={widgetContainerRef}></div>
-    );
+export default function Boeken() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+      <h1 className="text-3xl font-semibold text-gray-800 mb-4">
+        Boek jouw verblijf bij Villa Wildebras
+      </h1>
+      <BookingWidget />
+    </div>
+  );
 }
-
-export default BookingWidget;
