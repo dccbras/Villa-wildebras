d1a13ad147a89058b63ac92d57d4643a57c9af36
dccbras <dccbras@gmail.com>
2025-02-19 21:25:13 +0100

    Update page.tsx

--- app/boeken/page.tsx
+++ app/boeken/page.tsx
@@ -1,3 +1,4 @@
-export default function Test() {
-  return <div>Test Pagina</div>
-}
+<script src="//widget.simplybook.it/v2/widget/widget.js" type="text/javascript"></script>
+<script type="text/javascript">
+    var widget = new SimplybookWidget({"widget_type":"iframe","url":"https:\/\/villawildebras.simplybook.it","theme":"belle","theme_settings":{"timeline_hide_unavailable":"1","hide_past_days":"0","timeline_show_end_time":"0","timeline_modern_display":"as_slots","sb_base_color":"#7b8a78","display_item_mode":"block","sb_review_image":"","dark_font_color":"#474747","light_font_color":"#ffffff","btn_color_1":"#b0c3ac","sb_company_label_color":"#616760","hide_img_mode":"0","show_sidebar":"1","sb_busy":"#c7b3b3","sb_available":"#d6ebff"},"timeline":"modern","datepicker":"top_calendar","is_rtl":false,"app_config":{"clear_session":0,"allow_switch_to_ada":0,"predefined":[]}});
+</script>
