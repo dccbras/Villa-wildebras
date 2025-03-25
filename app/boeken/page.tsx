39f5d1099441465642a4a789e5c070f076e037ab
dccbras <dccbras@gmail.com>
2025-03-25 15:31:56 +0100

    Update page.tsx

--- app/boeken/page.tsx
+++ app/boeken/page.tsx
@@ -1,12 +1,1 @@
-import BookingWidget from "./BookingWidget";
 
-export default function Boeken() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
-      <h1 className="text-3xl font-semibold text-gray-800 mb-4">
-        Boek jouw verblijf bij Villa Wildebras
-      </h1>
-      <BookingWidget />
-    </div>
-  );
-}
