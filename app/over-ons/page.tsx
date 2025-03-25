b23261bb559f148f8f2ad1e920909bb6c246afe7
dccbras <dccbras@gmail.com>
2025-03-25 16:20:37 +0100

    Update page.tsx

--- app/over-ons/page.tsx
+++ app/over-ons/page.tsx
@@ -45,7 +45,7 @@ export default function Page() {
       {/* Header met afbeelding */}
       <div className="relative h-96 w-full">
         <Image
-          src="/uploads/WhatsApp Image 2025-03-25 at 15.36.17.jpeg"
+          src="zoe-gayah-jonker-tbvTNdviuc8-unsplash"
           alt="Tiny House Header"
           layout="fill"
           objectFit="cover"
