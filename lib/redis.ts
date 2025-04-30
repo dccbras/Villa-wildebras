8a27738260576207c0c943df807c985b3a9a5a84
dccbras <dccbras@gmail.com>
2025-04-30 22:12:36 +0200

    Update redis.ts

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,13 +1,3 @@
-import { Redis } from '@upstash/redis'
-
-const redis = new Redis({
-  url: 'https://tender-parakeet-22011.upstash.io',
-  token: 'AVX7AAIjcDFiZjFmY2I2YTYzMTI0NTM5YWY3Y2EwZDg1NzAzYTdiYnAxMA',
-})
-
-await redis.set('foo', 'bar');
-const data = await redis.get('foo');
-
 // lib/redis.ts
 import { createClient } from 'redis';
 
